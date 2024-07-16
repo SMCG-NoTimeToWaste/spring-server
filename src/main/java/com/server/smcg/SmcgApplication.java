de30e6f245c32b26c622f6d51ad7cd0e9d50f140
hg_yellow <jang010505@gmail.com>
2024-07-16 09:03:26 +0900

    init Project

---
+++ src/main/java/com/server/smcg/SmcgApplication.java
@@ -0,0 +1,13 @@
+package com.server.smcg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmcgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmcgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/server/smcg/SmcgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.smcg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmcgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

49a983481d16417454fe81bac2b6615d49eaf451
hg_yellow <jang010505@gmail.com>
2024-07-17 23:09:41 +0900

    feat: #1 Add ssh Tunneling

--- src/test/java/com/server/smcg/SmcgApplicationTests.java
+++ src/test/java/com/server/smcg/SmcgApplicationTests.java
@@ -2,8 +2,10 @@ package com.server.smcg;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SmcgApplicationTests {
 
 	@Test

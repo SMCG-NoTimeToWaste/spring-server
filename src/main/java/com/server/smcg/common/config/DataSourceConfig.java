3607e41b5048da12be168629563742a1134416e2
hg_yellow <63451752+jang010505@users.noreply.github.com>
2024-07-17 23:41:23 +0900

    Update DataSourceConfig.java

--- src/main/java/com/server/smcg/common/config/DataSourceConfig.java
+++ src/main/java/com/server/smcg/common/config/DataSourceConfig.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Profile;
 
 import javax.sql.DataSource;
 
-@Profile({"dev", "prod"})
+@Profile({"dev", "test"})
 @Configuration
 @RequiredArgsConstructor
 public class DataSourceConfig {
